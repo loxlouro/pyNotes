8f80106e13f5cab526ac00770226db485048ac7a
loxlouro <55482145+loxlouro@users.noreply.github.com>
2023-02-28 22:37:28 +0300

    Add files via upload

---
+++ notesAppP/notessAppP.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile, asksaveasfile
+
+file_name= NONE
+
+def new_file():
+    global file_name
+    file_name = "noname"
+    text.delete('1.0', END)
+
+def save_as():
+    out=asksaveasfile(mode='w', defaultextension='.txt')
+    data = text.get('1.0', END)
+    try:
+        out.write(data.rstrip())
+    except Exception:
+        messagebox.showerror("dats error bro with saving file")
+
+def open_file():
+    global file_name
+    inp = askopenfile(mode='r')
+    if inp is None:
+        return 
+        file_name = inp.name
+    data = inp.read()
+    text.delete('1.0', END)
+    text.insert('1.0, data')
+
+root = Tk()
+root.title("notesAppP")
+root.geometry("400x400")
+
+text= Text(root, width=400, height=400)
+text.pack()
+
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+
+file_menu.add_command(label="new", command=new_file)
+file_menu.add_command(label="open file", command= open_file)
+file_menu.add_command(label="save as", command=save_as)
+menu_bar.add_cascade(label ="file", menu=file_menu)
+
+root.config(menu=menu_bar)
+root.mainloop()
